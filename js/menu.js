03b6946c45ba05f021c7bd4797644a44a07c5add
InfernoDragon0 <mushroomapplepiele@gmail.com>
2022-06-21 18:23:14 +0800

    remove debug alert

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,5 @@
 document.getElementById("continueButton").addEventListener("click", () => {
     var selected = document.getElementById("selectCharacter").value
-    alert(selected)
 
     if (selected == "New Character") {
         window.location.href = "./index.html"
